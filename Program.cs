b899abc7dd888741050162cf00600bb571603444
Name <vbrfqlj@mail.ru>
2025-02-15 14:49:16 +0300

    Изменение нескольких параметров

--- Program.cs
+++ Program.cs
@@ -5,12 +5,15 @@ class Program
     static void Main(string[] args)
     {
         string name = "Евгения";
+        string oldname = "Евгения";
 
-        GetName(ref name);
+        GetName(out name, out oldname);
         Console.WriteLine(name);
+        Console.WriteLine(oldname);
     }
-    static void GetName(ref string name)
+    static void GetName(out string name, out string oldname)
     {
+        oldname = "Евгения";
         Console.WriteLine("Введите имя");
         name = Console.ReadLine();
 
